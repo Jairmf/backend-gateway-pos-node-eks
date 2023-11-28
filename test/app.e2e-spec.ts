104e38591cd74803fb2310d22ee4ba0a147c0610
Jair Masgo <jairmf_23@hotmail.com>
2023-11-27 23:57:14 -0500

    Add: Agregado endpoint helthcheck y archivos de despliegue

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,8 +17,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/healthcheck')
       .expect(200)
-      .expect('Hello World!');
+      .expect('OK!');
   });
 });
